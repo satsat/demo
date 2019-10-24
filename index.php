cdb3a12968c2707f769a7f0fd07e56638b24acd8
Satish Chauhan <chauhan.satish96@gmail.com>
2019-10-24 15:44:34 +0530

    added new pass in index.php

--- index.php
+++ index.php
@@ -3,5 +3,8 @@ echo "Password 1: ";
 echo crypt("Satish", 'SA')."<br/><br/>";
 echo "Password 2: ";
 echo password_hash("Satish", PASSWORD_DEFAULT)."<br/><br/>";
-
+$options = [
+    'cost' => 12,
+];
+echo "Password3: ".password_hash("rasmuslerdorf", PASSWORD_BCRYPT, $options);
 ?>
